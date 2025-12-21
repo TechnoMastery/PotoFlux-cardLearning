9d4b0dbce0a7627eca07fe5e258b1404813fb8ff
Minheur2000 <adrien.rousselet@hotmail.com>
2025-12-21 18:58:45 +0100

    Initial commit

---
+++ src/main/java/your/packagename/ExampleMod.java
@@ -0,0 +1,41 @@
+package your.packagename;
+
+import net.minheur.potoflux.PotoFlux;
+import net.minheur.potoflux.loader.PotoFluxLoadingContext;
+import net.minheur.potoflux.loader.mod.Mod;
+import net.minheur.potoflux.loader.mod.ModEventBus;
+import net.minheur.potoflux.loader.mod.events.RegisterLangEvent;
+import net.minheur.potoflux.loader.mod.events.RegisterTabsEvent;
+import your.packagename.tabs.Tabs;
+import your.packagename.translations.ExampleModTranslations;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Mod(modId = ExampleMod.MOD_ID)
+public class ExampleMod {
+    public static final String MOD_ID = "yourmodid";
+
+    public ExampleMod() {
+        ModEventBus modEventBus = PotoFluxLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(RegisterTabsEvent.class, this::onRegisterTabs);
+        modEventBus.addListener(RegisterLangEvent.class, this::onRegisterLang);
+    }
+
+    private void onRegisterTabs(RegisterTabsEvent event) {
+        Tabs.register(event.reg);
+    }
+    private void onRegisterLang(RegisterLangEvent event) {
+        event.registerLang(new ExampleModTranslations());
+    }
+
+    public static Path getModDir() {
+        Path dir = PotoFlux.getModDataDir().resolve(MOD_ID);
+        try {
+            Files.createDirectories(dir);
+        } catch (IOException ignored) {}
+        return dir;
+    }
+}
