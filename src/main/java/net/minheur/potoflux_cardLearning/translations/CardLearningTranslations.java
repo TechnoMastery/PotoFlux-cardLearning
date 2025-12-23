b60abaacc7a653907eecda5d3dd5bfc89de61104
Minheur4000 <ordi2526883@educ.normandie.fr>
2025-12-23 19:19:22 +0100

    added french translations

--- src/main/java/net/minheur/potoflux_cardLearning/translations/CardLearningTranslations.java
+++ src/main/java/net/minheur/potoflux_cardLearning/translations/CardLearningTranslations.java
@@ -11,75 +11,110 @@ public class CardLearningTranslations extends AbstractTranslationsRegistry {
     @Override
     protected void makeTranslation() {
         addCardTab("name")
-                .en("Card learning");
+                .en("Card learning")
+                .fr("Cartes mémoires");
         addCardTab("add_card")
-                .en("Add card");
+                .en("Add card")
+                .fr("Ajouter");
         addCardTab("available_lists")
-                .en("Available lists");
+                .en("Available lists")
+                .fr("Listes disponible");
         addCardTab("cancel_all")
-                .en("Cancel all");
+                .en("Cancel all")
+                .fr("Tout annuler");
         addCardTab("card_number")
-                .en("Number of cards: ");
+                .en("Number of cards: ")
+                .fr("Nombre de cartes : ");
         addCardTab("choose_list")
-                .en("Choose list");
+                .en("Choose list")
+                .fr("Choisissez un liste");
         addCardTab("delete_error")
-                .en("Error while deleting file: ");
+                .en("Error while deleting file: ")
+                .fr("Erreur lors de la suppression du fichier : ");
         addCardTab("details")
-                .en("Details of ");
+                .en("Details of ")
+                .fr("Détails de ");
         addCardTab("empty_list_valid")
-                .en("Empty list but enabled button !");
+                .en("Empty list but enabled button !")
+                .fr("Liste vide mais bouton activé !");
         addCardTab("error", "loading_list")
-                .en("Error while loading list.");
+                .en("Error while loading list.")
+                .fr("Erreur lors du chargement de la liste.");
         addCardTab("error", "reading_file")
-                .en("Error while reading file");
+                .en("Error while reading file")
+                .fr("Erreur lors de la lecture du fichier");
         addCardTab("error", "saving")
-                .en("Error while saving");
+                .en("Error while saving")
+                .fr("Erreur lors de la sauvegarde");
         addCardTab("export")
-                .en("Export list");
+                .en("Export list")
+                .fr("Exporter la liste");
         addCardTab("export", "error")
-                .en("Error while exporting: ");
+                .en("Error while exporting: ")
+                .fr("Erreur lors de l'export : ");
         addCardTab("export", "done")
-                .en("List exported successfully in: ");
+                .en("List exported successfully in: ")
+                .fr("Liste exportée avec succès dans : ");
         addCardTab("export", "done", "name")
-                .en("Exported successfully");
+                .en("Exported successfully")
+                .fr("Export réussi");
         addCardTab("face", "front")
-                .en("Front: ");
+                .en("Front: ")
+                .fr("Avant : ");
         addCardTab("face", "back")
-                .en("Back: ");
+                .en("Back: ")
+                .fr("Arrière : ");
         addCardTab("list", "invalid")
-                .en("Invalid or empty list");
+                .en("Invalid or empty list")
+                .fr("Liste invalide ou vide");
         addCardTab("list", "invalid", "name")
-                .en("This list's name is invalid.");
+                .en("This list's name is invalid.")
+                .fr("Le nom de la liste est vide.");
         addCardTab("list", "column")
-                .en("List: ");
+                .en("List: ")
+                .fr("Liste : ");
         addCardTab("list", "loaded")
-                .en("List loaded: ");
+                .en("List loaded: ")
+                .fr("Liste chargée : ");
         addCardTab("list", "name")
-                .en("List name: ");
+                .en("List name: ")
+                .fr("Nom de la liste : ");
         addCardTab("list", "delete", "confirm")
-                .en("List delection check");
+                .en("List delection check")
+                .fr("Validation de la suppression");
         addCardTab("list", "delete", "confirm", "dialog")
-                .en("Do you really want to delete the list ");
+                .en("Do you really want to delete the list ")
+                .fr("Voulez vraiment supprimer la liste ");
         addCardTab("list", "delete", "done", "start")
-                .en("The list ");
+                .en("The list ")
+                .fr("La liste ");
         addCardTab("list", "delete", "done", "end")
-                .en(" has been deleted.");
+                .en(" has been deleted.")
+                .fr(" à été supprimée");
         addCardTab("list", "saved")
-                .en("List saved successfully");
+                .en("List saved successfully")
+                .fr("Liste sauvegardée avec succès");
         addCardTab("list", "no_found")
-                .en("No lists found.");
+                .en("No lists found.")
+                .fr("Aucunes liste trouvée.");
         addCardTab("new")
-                .en("New card");
+                .en("New card")
+                .fr("Nouvelle carte");
         addCardTab("new", "empty")
-                .en("Both input should be filled correctly.");
+                .en("Both input should be filled correctly.")
+                .fr("Les deux entrés doivent être complétées correctement.");
         addCardTab("no_card")
-                .en("No cards added");
+                .en("No cards added")
+                .fr("Aucunes cartes ajoutée");
         addCardTab("no_selected")
-                .en("No card selected.");
+                .en("No card selected.")
+                .fr("Aucunes cartes sélectionnée");
         addCardTab("override")
-                .en("Do you want to override the cards ?");
+                .en("Do you want to override the cards ?")
+                .fr("Voulez vraiment écraser les cartes ?");
         addCardTab("createError")
-                .en("ERROR while creating tab ");
+                .en("ERROR while creating tab ")
+                .fr("ERREUR lors de la créatio de l'onglet ");
     }
 
     // tabs helper
