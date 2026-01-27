a176009fa6a4af02364187df5d0a0081cc4046b4
Minheur2000 <adrien.rousselet@hotmail.com>
2026-01-27 19:08:27 +0100

    added version getter

--- src/main/java/net/minheur/potoflux_cardLearning/CardLearningMod.java
+++ src/main/java/net/minheur/potoflux_cardLearning/CardLearningMod.java
@@ -5,12 +5,15 @@ import net.minheur.potoflux.loader.PotoFluxLoadingContext;
 import net.minheur.potoflux.loader.mod.Mod;
 import net.minheur.potoflux.loader.mod.ModEventBus;
 import net.minheur.potoflux.loader.mod.events.RegisterLangEvent;
+import net.minheur.potoflux.logger.LogCategories;
+import net.minheur.potoflux.logger.PtfLogger;
 import net.minheur.potoflux_cardLearning.tabs.Tabs;
 import net.minheur.potoflux_cardLearning.translations.CardLearningTranslations;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Properties;
 
 @Mod(modId = CardLearningMod.MOD_ID, version = "2.0", compatibleVersions = {"6.2"})
 public class CardLearningMod {
@@ -33,5 +36,18 @@ public class CardLearningMod {
             Files.createDirectories(dir);
         } catch (IOException ignored) {}
         return dir;
+    }
+
+    public static String getVersion() {
+        try {
+            Properties props = new Properties();
+            props.load(CardLearningMod.class.getResourceAsStream("/modVersion.properties"));
+
+            return props.getProperty("version");
+        } catch (IOException e) {
+            e.printStackTrace();
+            PtfLogger.error("Could not get version for mod " + MOD_ID, LogCategories.MOD_LOADER);
+            return null;
+        }
     }
 }
